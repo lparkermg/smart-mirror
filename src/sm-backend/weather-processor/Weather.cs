df10bbce3c044a5fd0dadc2b0cf2fd28742fb1b1
Luke Parker <lparker@moongate-uk.co.uk>
2015-05-21 10:52:37 +0100

    Setup and created WeatherObject.

---
+++ src/sm-backend/weather-processor/Weather.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace weatherprocessor
+{
+	public class Weather
+	{
+		public Weather ()
+		{
+		}
+
+		public string DownloadCurrentWeather(string cityId)
+		{
+			//Download the current weather using the City ID.
+
+		}
+
+		public string DownloadFivedayWeather(string cityId)
+		{
+			//Download the Fiveday weather forecast using the City ID.
+
+		}
+			
+		private void SaveToJson(string weatherToSave)
+		{
+			//Save the processed json to somewhere that the website can access it.
+
+		}
+	}
+}
+
---
+++ src/sm-backend/weather-processor/WeatherObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace weatherprocessor
+{
+	public class WeatherObject
+	{
+		public int CityId { get; private set;}
+		public string CityName { get; private set;}
+		public string Country { get; private set;}
+		public int WeatherId { get; private set;}
+		public string WeatherMain { get; private set;}
+		public string WeatherDescription { get; private set;}
+		public float CurrentTemp { get; private set;}
+		public float MinTemp { get; private set;}
+		public float MaxTemp { get; private set;}
+		public int Humidity { get; private set;}
+		public float WindSpeed { get; private set;}
+		public float WindDirection { get; private set;}
+
+		public WeatherObject (string weatherJson)
+		{
+			//Process the weather json into the object.
+		}
+	}
+}
+
